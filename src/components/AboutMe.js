0e97dbd464df5040db642081ee1ceea1730601e7
Maria SJ <maria.som.jur@gmail.com>
2023-02-13 13:24:17 +0100

    fixed css aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,6 +13,9 @@ const AboutMe = () => {
             </article>
 
             <article className="aboutMe__skills fade-in-bottom">
+                <div className="custom-shape-divider-bottom-1676289003">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path></svg>
+                </div>
                 <h2 className="aboutMe__title--two">Soft skills</h2>
                 <ul className="aboutMe__skillList">
                     <li className="aboutMe__skill"><i className="uil uil-adjust-circle aboutMe__icon"></i>Adaptabilidad</li>
@@ -24,6 +27,9 @@ const AboutMe = () => {
                     <li className="aboutMe__skill"><i className="uil uil-bullseye aboutMe__icon"></i>Atención al detalle</li>
                     <li className="aboutMe__skill"><i className="uil uil-bolt-alt aboutMe__icon"></i>Tolerancia a la presión</li>
                 </ul>
+                <div class="custom-shape-divider-top-1676289913">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path></svg>
+                </div>
             </article>
 
             <article className="aboutMe__tech fade-in-bottom">
