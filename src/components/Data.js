905d01dfe087bdc53faab0bea813ba404c576fb5
Maria SJ <maria.som.jur@gmail.com>
2023-03-13 13:24:44 +0100

    fixed cv download

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,7 +5,7 @@ const Data = (props) => {
         <div className="home__data fade-in-left">
             <h1 className="home__title">María Somoza</h1>
             <h3 className="home__subtitle">Frontend Developer</h3>
-            <a href="../.././CVMariaSJ.pdf" download target="_blank" className="home__button button--flex">Descargar CV</a>
+            <a href="../.././CVMariaSomoza.pdf" download target="_blank" className="home__button button--flex">Descargar CV</a>
             <NavLink href="#contact" className="home__button button--flex" to="/Contact">¿Charlamos?</NavLink>
         </div>
     );
