5579ae17f8da95d884f16ee2f97501180800f185
Maria SJ <maria.som.jur@gmail.com>
2023-03-13 16:49:31 +0100

    download cv

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,11 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import cvPdf from '../assets/CVMariaSomoza.pdf';
+
 const Data = (props) => {
 
     return (
         <div className="home__data fade-in-left">
             <h1 className="home__title">María Somoza</h1>
             <h3 className="home__subtitle">Frontend Developer</h3>
-            <a href="../.././CVMariaSomoza.pdf" download target="_blank" className="home__button button--flex">Descargar CV</a>
+            <a href={cvPdf} download="CVMariaSomoza" target="_blank" rel="noopener noreferrer" className="home__button button--flex">Descargar CV</a>
             <NavLink href="#contact" className="home__button button--flex" to="/Contact">¿Charlamos?</NavLink>
         </div>
     );
